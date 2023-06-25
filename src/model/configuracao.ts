ae9fbc49cba6c10b099997f61fcd51030c54049f
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-06-25 20:16:09 -0300

    iniciando configuraçao

--- src/model/configuracao.ts
+++ src/model/configuracao.ts
@@ -1,24 +1,14 @@
 import { abstractentity } from "@/model/abstractentity";
 
 export class configuracao extends abstractentity{
-   /*
-    valorhora!: number;
-    valorminuto!: number;
-    inicioexpediente!: string;
-    fimexpediente!: string;
-    tempoparadesconto!: string;
-    tempodedesconto!: string;
-    gerardesconto!: boolean;
-    vagasmotos!: number;
-    vagascarro!: number;
-    vagasvan!: number;*/
+  
 
     valorhora!: number;
     valorminuto!: number;
-    inicioexpediente!: number;
-    fimexpediente!: number;
-    tempoparadesconto!: number;
-    tempodedesconto!: number;
+    inicioexpediente!: Date;
+    fimexpediente!: Date;
+    tempoparadesconto!: Date;
+    tempodedesconto!: Date;
     gerardesconto!: boolean;
     vagasmotos!: number;
     vagascarro!: number;
